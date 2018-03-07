ca3e4af8e4d71bb3ae35d4588684ce0c23d28c1d
Irma <irblancofdz@gmail.com>
2018-03-07 21:58:42 +0100

    Add HouseDetailViewController

---
+++ AppFundamentos/Controllers/HouseDetailViewController.swift
@@ -0,0 +1,22 @@
+//
+//  HouseDetailViewController.swift
+//  AppFundamentos
+//
+//  Created by Irma Blanco on 07/03/2018.
+//  Copyright © 2018 Irma Blanco. All rights reserved.
+//
+
+import UIKit
+
+class HouseDetailViewController: UIViewController {
+
+    @IBOutlet weak var houseNameLabel: UILabel!
+    @IBOutlet weak var wordsLabel: UILabel!
+    @IBOutlet weak var sigilImageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
