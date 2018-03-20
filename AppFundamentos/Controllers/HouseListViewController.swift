6698b7428879a9dba2a49279bc38f57cdbfa6467
Irma <irblancofdz@gmail.com>
2018-03-20 21:09:47 +0100

    Add HouseListViewController

--- AppFundamentos/Controllers/HouseListViewController.swift
+++ AppFundamentos/Controllers/HouseListViewController.swift
@@ -9,43 +9,51 @@
 import UIKit
 
 class HouseListViewController: UITableViewController {
-        
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    
+    // Mark: - Properties
+    let model: [House]
+    
+    // Mark: - Initialization
+    init(model: [House]) {
+        self.model = model
+        super.init(style: .plain)
+        title = "Westeros"
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-
+  
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return model.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+        
+        let cellId = "HouseCell"
+        
+        //Descubrir cual es la casa que tenemos que mostrar
+        let house  = model[indexPath.row]
+        
+        //Crear una celda
+        var cell = tableView.dequeueReusableCell(withIdentifier: cellId)
+        if cell == nil {
+            cell = UITableViewCell(style: .default, reuseIdentifier: cellId)
+        }
+        //Sincronizar house (model) con cell /Vista
+        cell?.imageView?.image = house.sigil.image
+        cell?.textLabel?.text = house.name
+        
+        return cell!
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
