6ef07f4d746bb2696c1d5caa80a85f3f1b0111d8
Irma <irblancofdz@gmail.com>
2018-03-20 17:49:07 +0100

    Add WikiViewController

---
+++ AppFundamentos/Controllers/WikiViewController.swift
@@ -0,0 +1,26 @@
+//
+//  WikiViewController.swift
+//  AppFundamentos
+//
+//  Created by Irma Blanco on 20/03/2018.
+//  Copyright © 2018 Irma Blanco. All rights reserved.
+//
+
+import UIKit
+import WebKit
+class WikiViewController: UIViewController {
+
+    @IBOutlet weak var webView: WKWebView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
